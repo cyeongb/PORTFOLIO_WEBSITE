5a37d1985dba567c97d258e58e2f045b101b9c7f
choeyeongbi <cyeongb@gmail.com>
2022-02-08 21:25:50 +0900

    스크롤 시 헤더에 이벤트 등록함

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/* ===================== Change Background Header ===================== */
+
+// 스크롤 viewport수치에 따라 50이상이면 scroll-header라는 클래스명을 붙여줍니다.
+
+function scrollHeader() {
+  const header = document.getElementById("header");
+
+  if (this.scrollY >= 50) {
+    console.log("동작", this.scrollY);
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+}
+window.addEventListener("scroll", scrollHeader);
+
+/* ===================== Service Modal ===================== */
+
+/* ===================== Mix it up Filter Portfolio ===================== */
+
+/* link active  */
+
+/* ===================== Swiper Testimonial ===================== */
+
+/* ===================== Scroll Section Active Link ===================== */
