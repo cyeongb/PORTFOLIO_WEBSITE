5be14465f568030b9c3d0bd691f254d193e4cc41
choeyeongbi <cyeongb@gmail.com>
2022-03-08 22:40:45 +0900

    서비스 모달창 구현 작업

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ function scrollHeader() {
   const header = document.getElementById("header");
 
   if (this.scrollY >= 50) {
-    console.log("동작", this.scrollY);
+    // console.log("동작", this.scrollY);
     header.classList.add("scroll-header");
   } else {
     header.classList.remove("scroll-header");
